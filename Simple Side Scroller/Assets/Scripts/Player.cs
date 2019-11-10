0d82a17a9e6885dc7a1711057409347372b7e482
clockwerkz <clockwerkz@gmail.com>
2019-11-09 17:04:52 -0800

    Hooks up player char to controls, adds rough 2d collision

---
+++ Simple Side Scroller/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    public float speed=1;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        //Get player's input
+        float input = Input.GetAxisRaw("Horizontal");
+        
+        //Moving player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+	}
+}
