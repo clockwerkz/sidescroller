e8d45ea91ea3f02adab5dbea0dfa6d12eb35fae4
clockwerkz <clockwerkz@gmail.com>
2019-11-14 23:12:22 -0800

    Adds enemy collision detection

---
+++ Simple Side Scroller/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float minSpeed = 2f;
+    public float maxSpeed = 10f;
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(minSpeed, maxSpeed); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter2D(Collider2D hitObject)
+    {
+        if (hitObject.tag == "Player")
+        {
+            print("We hit the player!");
+        }
+    }
+}
