039466c4e4eb8e6936920397ea732ac858bf7d20
clockwerkz <clockwerkz@gmail.com>
2019-11-22 23:49:33 -0800

    Adds random spawn points for the enemies

---
+++ Simple Side Scroller/Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+    public GameObject[] hazards;
+
+    private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+
+	// Update is called once per frame
+	void Update () {
+        if (timeBtwSpawns <= 0)
+        {
+            Transform rndSpawnPoint = spawnPoints[Random.Range(0, 8)];
+            GameObject rndHazard = hazards[0];
+            Instantiate(rndHazard, rndSpawnPoint.position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+        } else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+	}
+}
