a8d91301ed9173f8858af6f64d1b27247e706016
Carlos Fins <clockwerkz@gmail.com>
2019-12-14 21:25:53 -0800

    fixes null reference exception error when player is not in scene

--- Simple Side Scroller/Assets/Scripts/Spawner.cs
+++ Simple Side Scroller/Assets/Scripts/Spawner.cs
@@ -10,18 +10,32 @@ public class Spawner : MonoBehaviour {
     private float timeBtwSpawns = 0;
     public float startTimeBtwSpawns;
 
-	// Update is called once per frame
-	void Update () {
-        if (timeBtwSpawns <= 0)
-        {
-            Transform rndSpawnPoint = spawnPoints[Random.Range(0, 8)];
+    public float minTimeBetweenSpawns;
+    public float decrease;
 
-            GameObject rndHazard = hazards[0];
-            Instantiate(rndHazard, rndSpawnPoint.position, Quaternion.identity);
-            timeBtwSpawns = startTimeBtwSpawns;
-        } else
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player)
         {
-            timeBtwSpawns -= Time.deltaTime;
+            if (timeBtwSpawns <= 0)
+            {
+                Transform rndSpawnPoint = spawnPoints[Random.Range(0, 8)];
+
+                GameObject rndHazard = hazards[0];
+                Instantiate(rndHazard, rndSpawnPoint.position, Quaternion.identity);
+                if (startTimeBtwSpawns > minTimeBetweenSpawns)
+                {
+                    startTimeBtwSpawns -= decrease;
+                }
+                timeBtwSpawns = startTimeBtwSpawns;
+            }
+            else
+            {
+                timeBtwSpawns -= Time.deltaTime;
+            }
         }
-	}
+    }
 }
